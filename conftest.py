45f35b39bc350b549b3578aeeb898e4f988762eb
Tatyana Evtikhova <tatyanaevtihova@gmail.com>
2024-05-18 11:10:18 +0300

    Chrome driver updated

--- conftest.py
+++ conftest.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import pytest
 from pages.sale import Sale
@@ -10,6 +12,10 @@ from pages.create_account import AccountCreation
 def driver():
     options = Options()
     options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920x1080')
+    chrome_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     chrome_driver = webdriver.Chrome()
     return chrome_driver
 

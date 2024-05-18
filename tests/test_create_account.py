5f61a49c8135ecd202737ca62824ca9e23bb9085
Tatyana Evtikhova <tatyanaevtihova@gmail.com>
2024-05-18 10:55:03 +0300

    updated tests

--- tests/test_create_account.py
+++ tests/test_create_account.py
@@ -1,17 +1,15 @@
-@pytest.mark.smoke
+
 def test_successful_registration(login_page):
     login_page.open_page()
     login_page.fill_registration_form("Test", "User", "Test1234", "Test1234")
     login_page.check_successful_registration()
 
 
-@pytest.mark.smoke
 def test_guest_post_message(login_page):
     login_page.open_page()
     login_page.check_guest_message()
 
 
-@pytest.mark.regression
 def test_email_validation(login_page):
     login_page.open_page()
     login_page.invalid_email()
--- tests/test_eco_friendly.py
+++ tests/test_eco_friendly.py
@@ -1,21 +1,18 @@
 import pytest
 
 
-@pytest.mark.extended
 def test_descending_order(ecofriendly_page):
     ecofriendly_page.open_page()
     ecofriendly_page.sort_items_by_name_descending()
     ecofriendly_page.check_first_item_after_sorting_descending()
 
 
-@pytest.mark.extended
 def test_sorting(ecofriendly_page):
     ecofriendly_page.open_page()
     ecofriendly_page.sort_items_by_price()
     ecofriendly_page.check_first_item_after_sorting()
 
 
-@pytest.mark.smoke
 def test_switch_view(ecofriendly_page):
     ecofriendly_page.open_page()
     ecofriendly_page.check_initial_toolbar_text()
